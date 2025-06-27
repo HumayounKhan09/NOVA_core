0bad15ee60b2b1999828e60840560d1b89cc18e8
Humayoun Khan <humayounkhan09@outlook.com>
2025-06-27 12:43:15 -0400

    finished the scheduler

--- scheduler.py
+++ scheduler.py
@@ -2,4 +2,5 @@
     This schedules and actually operates the reminder schedules
     """
     from loader import load_reminders
+    from apscheduler.schedulers.background import BackgroundScheduler
     
