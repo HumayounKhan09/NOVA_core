f2321934bb5c50a30d0c8c32ec8e6073ebba729a
Humayoun Khan <humayounkhan09@outlook.com>
2025-06-27 13:13:15 -0400

    Driver for scheduler1

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,30 @@
     """
     This schedules and actually operates the reminder schedules
     """
-    from loader import load_reminders
     from apscheduler.schedulers.background import BackgroundScheduler
-    
+    from plyer import notification 
+
+    #Need to set up call back
+    def run_reminders(reminder):
+        notification.notify(
+            title = "NOVA Reminder",
+            message=reminder.message,
+            timeout = 10
+        )
+    #Setting up the sched function
+    def schedule_reminders(loaded_reminders):
+        scheduler = BackgroundScheduler()
+        for rem in loaded_reminders:
+            if rem.enabled:
+                scheduler.add_job(
+                func= run_reminders,
+                trigger=rem.trigger,
+                args = [rem],
+                id = rem.id
+                )
+        scheduler.start()
+        try:
+            while True:
+                sleep()
+        except KeyboardInterrupt:
+            scheduler.shutdown()
